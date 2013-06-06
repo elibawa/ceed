22a9db2507aba8115e2df52ecaa0cb50fac982c2
kelvin bawa <elibawa@yahoo.com>
2013-06-06 10:20:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ceed::Application.config.secret_token = 'd5fe06ef49f4be7342565f41bd5ff4dbb542702101241a6e0800aa0a6866b8b77a9bba655265cdaa07966f63f63fdf5f6ee50f15a88b89eeeba15dbbb2afd1c5'
